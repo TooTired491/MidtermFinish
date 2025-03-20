5ee6b0d5d52f15df5a9aa3b0f2237d6c2f71f145
TooTired491 <Insomniaccat491@gmail.com>
2025-03-20 09:28:41 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Untitled
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/random-nobody/pen/EaxZWvR](https://codepen.io/random-nobody/pen/EaxZWvR).
+
---
+++ script.js
@@ -0,0 +1,24 @@
+//TPolk
+let hoverElement1 = document.getElementById('button');
+hoverElement1.addEventListener('mouseenter', function() {
+	hoverElement1.style.opacity = '0.8';
+})
+hoverElement1.addEventListener('mouseleave', function() {
+	hoverElement1.style.opacity = '3';
+});
+//Rangels
+let hoverElement2 = document.getElementById('button2'); 
+hoverElement2.addEventListener('mouseenter', function() {
+	hoverElement2.style.opacity = '0.8';
+})
+hoverElement2.addEventListener('mouseleave', function() {
+	hoverElement2.style.opacity = '1';
+});
+//Paul
+let hoverElement3 = document.getElementById('button3'); 
+hoverElement3.addEventListener('mouseenter', function() {
+	hoverElement3.style.opacity = '0.8';
+})
+hoverElement3.addEventListener('mouseleave', function() {
+	hoverElement3.style.opacity = '1';
+});
